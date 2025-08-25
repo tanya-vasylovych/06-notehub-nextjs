a6506e9201820de8db67a10a43a00b8b608b60be
Tanya Vasylovych <tetiana.vasylovych.work@gmail.com>
2025-08-25 20:53:35 +0300

    settings

---
+++ lib/api.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+import type { CreateNote, Note } from "../types/note.ts";
+
+const instance = axios.create({
+  baseURL: "https://notehub-public.goit.study/api",
+  headers: { Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}` },
+});
+
+interface FetchNotesResponse {
+  notes: Note[];
+  totalPages: number;
+}
+
+export async function fetchNotes({
+  page,
+  search,
+}: {
+  page: number;
+  search: string;
+}): Promise<FetchNotesResponse> {
+  const { data } = await instance.get<FetchNotesResponse>("/notes", {
+    params: { page, search, perPage: 12 },
+  });
+  return data;
+}
+
+export async function createNote(newNote: CreateNote): Promise<Note> {
+  const { data } = await instance.post<Note>("/notes", newNote);
+  return data;
+}
+
+export async function deleteNote(id: string): Promise<Note> {
+  const { data } = await instance.delete<Note>(`/notes/${id}`);
+  return data;
+}
